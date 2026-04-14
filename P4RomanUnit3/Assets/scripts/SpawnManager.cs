d85bb8498787aa364eee5fc26834c62945d2907b
informalboi <733416@student.dusd.net>
2026-04-13 23:03:20 -0700

    fsd

--- P4RomanUnit3/Assets/scripts/SpawnManager.cs
+++ P4RomanUnit3/Assets/scripts/SpawnManager.cs
@@ -6,15 +6,20 @@ public class SpawnManager : MonoBehaviour
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 1;
+    private PlayerController playerControllerScript;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (playerControllerScript.gameOver == false)
+        
+      
         Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
